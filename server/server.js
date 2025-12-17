16d2c4b4150f300044a8118e4fc539b6b5497b3a
rmoraes-git <contatoimediato@gmail.com>
2025-12-16 21:57:12 -0300

    melhorias supabase

--- server/server.js
+++ server/server.js
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, "127.0.0.1", () => {
-  console.log("✅ Servidor rodando em http://127.0.0.1:3000");
+app.listen(PORT,  () => {
+  console.log("✅ Servidor rodando ");
 });
